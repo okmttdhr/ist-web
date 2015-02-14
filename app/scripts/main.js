d64562dfb56f290674dda8434b41aad2a2ec9d57
okmttdhr <10101114fin@gmail.com>
2015-02-14 15:01:08 +0900

    両端クリックで真ん中に配置が変わるようにしました

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,37 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+var mainJs = {
+  init: function() {
+    var self = this;
+    self.bind();
+  },
+  bind: function() {
+    var self = this;
+
+    $('body').on('click', '.ist__project__one', function() {
+      self.changePotision($(this));
+    });
+  },
+  changePotision: function(target) {
+    var istProjectOneCenter = $('.center');
+    if (target.hasClass('center')) {
+
+    } else {
+
+      target.addClass('center');
+      istProjectOneCenter.removeClass('center');
+
+      if (target.hasClass('left')) {
+        target.removeClass('left');
+        istProjectOneCenter.addClass('left');
+      }
+
+      if (target.hasClass('right')) {
+        target.removeClass('right');
+        istProjectOneCenter.addClass('right');
+      }
+    }
+  }
+};
+
+mainJs.init();
