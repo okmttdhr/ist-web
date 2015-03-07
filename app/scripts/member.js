99a09ff020861fb445aab6840878e1440354c76e
okmttdhr <10101114fin@gmail.com>
2015-03-07 17:39:09 +0900

    orientation apiで動かせるように（要実機チェック）

---
+++ app/scripts/member.js
@@ -0,0 +1,82 @@
+'use strict';
+
+function MemberJs() {
+  var rotateDegY = 0;
+  var rotateDegX = 0;
+
+  this.init = function() {
+    var self = this;
+    self.bind();
+    setInterval( function() { self.rotateLoop(); }, 1000/60 );
+
+    // #todo showを実行しないときの仮置き。消す。
+    $('#ist__member__carousel--js').velocity({
+      translateZ: '-300px',
+      rotateX: '0deg',
+      rotateY: '0deg',
+    });
+  };
+
+  this.bind = function() {
+    var self = this;
+
+    // $('body, html').on('mousemove', function(e) {
+    //   self.rotateMouseover(e);
+    // });
+
+    window.addEventListener('deviceorientation', self.rotateDeviceOrientation);
+  };
+
+  // this.deviceOrientationListener = function(e) {
+  //   var self = this;
+  //   console.log('---------------');
+  //   console.log(e.beta);
+  //   console.log(e.gamma);
+  //   self.rotateDeviceOrientation(e);
+  // };
+
+  this.show = function() {
+    // velocityを最初に適応する一瞬translateZが無効化しぶれるので、ロード時にstyleを追加しておく
+    $('#ist__member__carousel--js').velocity({
+      translateZ: '-300px',
+      rotateX: '0deg',
+      rotateY: '0deg'
+    });
+
+    // 瞬間間を置いて出現
+    setTimeout(function() {
+      $('.ist__member').velocity('fadeIn', { duration: 250, easing: 'ease' });
+    }, 800);
+  };
+
+  this.rotateLoop = function() {
+    $('#ist__member__carousel--js').velocity({
+      translateZ: '-300px',
+      rotateX: rotateDegX + 'deg',
+      rotateY: '+=' + rotateDegY + 'deg'
+    }, {
+      duration: 0,
+      easing: 'ease'
+    });
+  };
+
+  this.rotateMouseover = function(e) {
+    var win = $(window);
+    var windowWidth = win.width();
+    var windowHeight = win.height();
+    var speedRateY = 0.002;
+    var speedRateX = 0.05;
+    rotateDegY = -(e.clientX - (windowWidth*0.5)) * speedRateY;
+    rotateDegX = -(e.clientY - (windowHeight*0.5)) * speedRateX;
+  };
+
+  this.rotateDeviceOrientation = function(e) {
+    console.log(e.beta);
+    console.log(e.gamma);
+    var speedRateY = 0.1;
+    var speedRateX = 2;
+
+    rotateDegY = -(e.gamma) * speedRateY;
+    rotateDegX = -(e.beta) * speedRateX;
+  };
+}
